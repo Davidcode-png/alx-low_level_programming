d6941dcc00365acc0fd0ba681c5f2fcae659acae
Ojewale Opeoluwa <74027215+Davidcode-png@users.noreply.github.com>
2022-09-13 22:36:20 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints _putchar, fllowd by a new linees
+ * Return: Alwys 0 (Success)
  */
+int main(void)
+{
+	char variable[] = "_putchar";
 
-int main(void){
-    char variable[10] = "_putchar";
-    
-    int i;
-    
-    for(i = 0, i< 8,i++){
-        _putchar(school[i])
-    }
-    
-    _putchar(10);
-    return (0);
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(variable[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
