763b7bf81f9b6148bfbf038ba458d734d8e970aa
DavidsonCo <opeoluwa313@gmail.com>
2022-09-13 22:28:45 +0100

    Function Task 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void){
+    printf("_putchar\n")
+}
