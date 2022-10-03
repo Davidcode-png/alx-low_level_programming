6ee667e333ce27dc1fee9d264fbbb80694d214ac
Ojewale Opeoluwa <74027215+Davidcode-png@users.noreply.github.com>
2022-10-03 18:29:44 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - prints the number of arguments
-*@argc: number of arguments
-*@argv:array of arguments
-*Return: the number of arguments
-*/
-
-int main(int argc, char __attribute__((__unused__)) *argv[])
+  * main - prints its name, followed by new.
+  * @argc: argument counter.
+  * @argv: argument vector.
+  * Return: zero.
+  */
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
